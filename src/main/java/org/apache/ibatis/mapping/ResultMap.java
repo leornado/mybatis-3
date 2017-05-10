3f1f636702a6e70281114961dd9aa95bd890cd51
leornado <leornado@126.com>
2017-05-10 14:08:37 +0800

    新增动态<result column="${exp}">ognl表达式支持 新增动态sql中名为'_variables'参数集合（来源configuration.variables）

--- src/main/java/org/apache/ibatis/mapping/ResultMap.java
+++ src/main/java/org/apache/ibatis/mapping/ResultMap.java
@@ -15,147 +15,171 @@
  */
 package org.apache.ibatis.mapping;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.Set;
-
 import org.apache.ibatis.session.Configuration;
+
+import java.util.*;
 
 /**
  * @author Clinton Begin
  */
 public class ResultMap {
-  private String id;
-  private Class<?> type;
-  private List<ResultMapping> resultMappings;
-  private List<ResultMapping> idResultMappings;
-  private List<ResultMapping> constructorResultMappings;
-  private List<ResultMapping> propertyResultMappings;
-  private Set<String> mappedColumns;
-  private Discriminator discriminator;
-  private boolean hasNestedResultMaps;
-  private boolean hasNestedQueries;
-  private Boolean autoMapping;
 
-  private ResultMap() {
-  }
-
-  public static class Builder {
-    private ResultMap resultMap = new ResultMap();
-
-    public Builder(Configuration configuration, String id, Class<?> type, List<ResultMapping> resultMappings) {
-      this(configuration, id, type, resultMappings, null);
+    private ResultMap() {
     }
 
-    public Builder(Configuration configuration, String id, Class<?> type, List<ResultMapping> resultMappings, Boolean autoMapping) {
-      resultMap.id = id;
-      resultMap.type = type;
-      resultMap.resultMappings = resultMappings;
-      resultMap.autoMapping = autoMapping;
+    public void forceNestedResultMaps() {
+        hasNestedResultMaps = true;
     }
 
-    public Builder discriminator(Discriminator discriminator) {
-      resultMap.discriminator = discriminator;
-      return this;
+    public Boolean getAutoMapping() {
+        return autoMapping;
     }
 
-    public Class<?> type() {
-      return resultMap.type;
+    public List<ResultMapping> getConstructorResultMappings() {
+        return constructorResultMappings;
     }
 
-    public ResultMap build() {
-      if (resultMap.id == null) {
-        throw new IllegalArgumentException("ResultMaps must have an id");
-      }
-      resultMap.mappedColumns = new HashSet<String>();
-      resultMap.idResultMappings = new ArrayList<ResultMapping>();
-      resultMap.constructorResultMappings = new ArrayList<ResultMapping>();
-      resultMap.propertyResultMappings = new ArrayList<ResultMapping>();
-      for (ResultMapping resultMapping : resultMap.resultMappings) {
-        resultMap.hasNestedQueries = resultMap.hasNestedQueries || resultMapping.getNestedQueryId() != null;
-        resultMap.hasNestedResultMaps = resultMap.hasNestedResultMaps || (resultMapping.getNestedResultMapId() != null && resultMapping.getResultSet() == null);
-        final String column = resultMapping.getColumn();
-        if (column != null) {
-          resultMap.mappedColumns.add(column.toUpperCase(Locale.ENGLISH));
-        } else if (resultMapping.isCompositeResult()) {
-          for (ResultMapping compositeResultMapping : resultMapping.getComposites()) {
-            final String compositeColumn = compositeResultMapping.getColumn();
-            if (compositeColumn != null) {
-              resultMap.mappedColumns.add(compositeColumn.toUpperCase(Locale.ENGLISH));
+    public Discriminator getDiscriminator() {
+        return discriminator;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<ResultMapping> getIdResultMappings() {
+        return idResultMappings;
+    }
+
+    public Set<String> getMappedColumns() {
+        return mappedColumns;
+    }
+
+    public Map<String, String> getMappedRawColumnsMapper() {
+        return mappedRawColumnsMapper;
+    }
+
+    public List<ResultMapping> getPropertyResultMappings() {
+        return propertyResultMappings;
+    }
+
+    public List<ResultMapping> getResultMappings() {
+        return resultMappings;
+    }
+
+    public Class<?> getType() {
+        return type;
+    }
+
+    public boolean hasNestedQueries() {
+        return hasNestedQueries;
+    }
+
+    public boolean hasNestedResultMaps() {
+        return hasNestedResultMaps;
+    }
+
+    public ResultMap setMappedRawColumnsMapper(Map<String, String> mappedRawColumnsMapper) {
+        this.mappedRawColumnsMapper = mappedRawColumnsMapper;
+        return this;
+    }
+
+    private Boolean autoMapping;
+
+    private Class<?> type;
+
+    private Discriminator discriminator;
+
+    private List<ResultMapping> constructorResultMappings;
+
+    private List<ResultMapping> idResultMappings;
+
+    private List<ResultMapping> propertyResultMappings;
+
+    private List<ResultMapping> resultMappings;
+
+    private Map<String, String> mappedRawColumnsMapper;
+
+    private Set<String> mappedColumns;
+
+    private String id;
+
+    private boolean hasNestedQueries;
+
+    private boolean hasNestedResultMaps;
+
+    public static class Builder {
+
+        public Builder(Configuration configuration, String id, Class<?> type, List<ResultMapping> resultMappings) {
+            this(configuration, id, type, resultMappings, null);
+        }
+
+        public Builder(Configuration configuration, String id, Class<?> type, List<ResultMapping> resultMappings, Boolean autoMapping) {
+            resultMap.id = id;
+            resultMap.type = type;
+            resultMap.resultMappings = resultMappings;
+            resultMap.autoMapping = autoMapping;
+        }
+
+        public ResultMap build() {
+            if (resultMap.id == null) {
+                throw new IllegalArgumentException("ResultMaps must have an id");
             }
-          }
+            resultMap.mappedColumns = new HashSet<String>();
+            resultMap.mappedRawColumnsMapper = new HashMap<String, String>();
+            resultMap.idResultMappings = new ArrayList<ResultMapping>();
+            resultMap.constructorResultMappings = new ArrayList<ResultMapping>();
+            resultMap.propertyResultMappings = new ArrayList<ResultMapping>();
+            for (ResultMapping resultMapping : resultMap.resultMappings) {
+                resultMap.hasNestedQueries = resultMap.hasNestedQueries || resultMapping.getNestedQueryId() != null;
+                resultMap.hasNestedResultMaps = resultMap.hasNestedResultMaps || (resultMapping.getNestedResultMapId() != null && resultMapping.getResultSet() == null);
+                final String column = resultMapping.getColumn();
+                if (column != null) {
+                    String mappedColumn = column.toUpperCase(Locale.ENGLISH);
+                    resultMap.mappedColumns.add(mappedColumn);
+                    resultMap.mappedRawColumnsMapper.put(mappedColumn, column);
+                } else if (resultMapping.isCompositeResult()) {
+                    for (ResultMapping compositeResultMapping : resultMapping.getComposites()) {
+                        final String compositeColumn = compositeResultMapping.getColumn();
+                        if (compositeColumn != null) {
+                            String mappedColumn = compositeColumn.toUpperCase(Locale.ENGLISH);
+                            resultMap.mappedColumns.add(mappedColumn);
+                            resultMap.mappedRawColumnsMapper.put(mappedColumn, compositeColumn);
+                        }
+                    }
+                }
+                if (resultMapping.getFlags().contains(ResultFlag.CONSTRUCTOR)) {
+                    resultMap.constructorResultMappings.add(resultMapping);
+                } else {
+                    resultMap.propertyResultMappings.add(resultMapping);
+                }
+                if (resultMapping.getFlags().contains(ResultFlag.ID)) {
+                    resultMap.idResultMappings.add(resultMapping);
+                }
+            }
+            if (resultMap.idResultMappings.isEmpty()) {
+                resultMap.idResultMappings.addAll(resultMap.resultMappings);
+            }
+            // lock down collections
+            resultMap.resultMappings = Collections.unmodifiableList(resultMap.resultMappings);
+            resultMap.idResultMappings = Collections.unmodifiableList(resultMap.idResultMappings);
+            resultMap.constructorResultMappings = Collections.unmodifiableList(resultMap.constructorResultMappings);
+            resultMap.propertyResultMappings = Collections.unmodifiableList(resultMap.propertyResultMappings);
+            resultMap.mappedColumns = Collections.unmodifiableSet(resultMap.mappedColumns);
+            return resultMap;
         }
-        if (resultMapping.getFlags().contains(ResultFlag.CONSTRUCTOR)) {
-          resultMap.constructorResultMappings.add(resultMapping);
-        } else {
-          resultMap.propertyResultMappings.add(resultMapping);
+
+        public Builder discriminator(Discriminator discriminator) {
+            resultMap.discriminator = discriminator;
+            return this;
         }
-        if (resultMapping.getFlags().contains(ResultFlag.ID)) {
-          resultMap.idResultMappings.add(resultMapping);
+
+        public Class<?> type() {
+            return resultMap.type;
         }
-      }
-      if (resultMap.idResultMappings.isEmpty()) {
-        resultMap.idResultMappings.addAll(resultMap.resultMappings);
-      }
-      // lock down collections
-      resultMap.resultMappings = Collections.unmodifiableList(resultMap.resultMappings);
-      resultMap.idResultMappings = Collections.unmodifiableList(resultMap.idResultMappings);
-      resultMap.constructorResultMappings = Collections.unmodifiableList(resultMap.constructorResultMappings);
-      resultMap.propertyResultMappings = Collections.unmodifiableList(resultMap.propertyResultMappings);
-      resultMap.mappedColumns = Collections.unmodifiableSet(resultMap.mappedColumns);
-      return resultMap;
+
+        private ResultMap resultMap = new ResultMap();
+
     }
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public boolean hasNestedResultMaps() {
-    return hasNestedResultMaps;
-  }
-
-  public boolean hasNestedQueries() {
-    return hasNestedQueries;
-  }
-
-  public Class<?> getType() {
-    return type;
-  }
-
-  public List<ResultMapping> getResultMappings() {
-    return resultMappings;
-  }
-
-  public List<ResultMapping> getConstructorResultMappings() {
-    return constructorResultMappings;
-  }
-
-  public List<ResultMapping> getPropertyResultMappings() {
-    return propertyResultMappings;
-  }
-
-  public List<ResultMapping> getIdResultMappings() {
-    return idResultMappings;
-  }
-
-  public Set<String> getMappedColumns() {
-    return mappedColumns;
-  }
-
-  public Discriminator getDiscriminator() {
-    return discriminator;
-  }
-
-  public void forceNestedResultMaps() {
-    hasNestedResultMaps = true;
-  }
-  
-  public Boolean getAutoMapping() {
-    return autoMapping;
-  }
 
 }
--- src/main/java/org/apache/ibatis/scripting/xmltags/DynamicContext.java
+++ src/main/java/org/apache/ibatis/scripting/xmltags/DynamicContext.java
@@ -15,121 +15,137 @@
  */
 package org.apache.ibatis.scripting.xmltags;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import ognl.OgnlException;
 import ognl.OgnlRuntime;
 import ognl.PropertyAccessor;
-
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.session.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Clinton Begin
  */
 public class DynamicContext {
 
-  public static final String PARAMETER_OBJECT_KEY = "_parameter";
-  public static final String DATABASE_ID_KEY = "_databaseId";
-
-  static {
-    OgnlRuntime.setPropertyAccessor(ContextMap.class, new ContextAccessor());
-  }
-
-  private final ContextMap bindings;
-  private final StringBuilder sqlBuilder = new StringBuilder();
-  private int uniqueNumber = 0;
-
-  public DynamicContext(Configuration configuration, Object parameterObject) {
-    if (parameterObject != null && !(parameterObject instanceof Map)) {
-      MetaObject metaObject = configuration.newMetaObject(parameterObject);
-      bindings = new ContextMap(metaObject);
-    } else {
-      bindings = new ContextMap(null);
-    }
-    bindings.put(PARAMETER_OBJECT_KEY, parameterObject);
-    bindings.put(DATABASE_ID_KEY, configuration.getDatabaseId());
-  }
-
-  public Map<String, Object> getBindings() {
-    return bindings;
-  }
-
-  public void bind(String name, Object value) {
-    bindings.put(name, value);
-  }
-
-  public void appendSql(String sql) {
-    sqlBuilder.append(sql);
-    sqlBuilder.append(" ");
-  }
-
-  public String getSql() {
-    return sqlBuilder.toString().trim();
-  }
-
-  public int getUniqueNumber() {
-    return uniqueNumber++;
-  }
-
-  static class ContextMap extends HashMap<String, Object> {
-    private static final long serialVersionUID = 2977601501966151582L;
-
-    private MetaObject parameterMetaObject;
-    public ContextMap(MetaObject parameterMetaObject) {
-      this.parameterMetaObject = parameterMetaObject;
+    public DynamicContext(Configuration configuration, Object parameterObject) {
+        if (parameterObject != null && !(parameterObject instanceof Map)) {
+            MetaObject metaObject = configuration.newMetaObject(parameterObject);
+            bindings = new ContextMap(metaObject);
+        } else {
+            bindings = new ContextMap(null);
+        }
+        bindings.put(PARAMETER_OBJECT_KEY, parameterObject);
+        bindings.put(DATABASE_ID_KEY, configuration.getDatabaseId());
+        bindings.put(VARIABLES_KEY, configuration.getVariables());
     }
 
-    @Override
-    public Object put(String key, Object value) {
-      return super.put(key, value);
+    public void appendSql(String sql) {
+        sqlBuilder.append(sql);
+        sqlBuilder.append(" ");
     }
-    
-    @Override
-    public Object get(Object key) {
-      String strKey = (String) key;
-      if (super.containsKey(strKey)) {
-        return super.get(strKey);
-      }
 
-      if (parameterMetaObject != null) {
-        Object object = parameterMetaObject.getValue(strKey);
-        // issue #61 do not modify the context when reading
+    public void bind(String name, Object value) {
+        bindings.put(name, value);
+    }
+
+    public Map<String, Object> getBindings() {
+        return bindings;
+    }
+
+    public String getSql() {
+        return sqlBuilder.toString().trim();
+    }
+
+    public int getUniqueNumber() {
+        return uniqueNumber++;
+    }
+
+    private int uniqueNumber = 0;
+
+    public static final String DATABASE_ID_KEY = "_databaseId";
+
+    public static final String PARAMETER_OBJECT_KEY = "_parameter";
+
+    public static final String VARIABLES_KEY = "_variables";
+
+    private final ContextMap bindings;
+
+    private final StringBuilder sqlBuilder = new StringBuilder();
+
+    static {
+        OgnlRuntime.setPropertyAccessor(ContextMap.class, new ContextAccessor());
+    }
+
+    static class ContextAccessor implements PropertyAccessor {
+
+        public Object getProperty(Map context, Object target, Object name)
+                throws OgnlException {
+            Map map = (Map) target;
+
+            Object result = map.get(name);
+            if (result != null) {
+                return result;
+            }
+
+            Object parameterObject = map.get(PARAMETER_OBJECT_KEY);
+            if (parameterObject instanceof Map) {
+                result = ((Map) parameterObject).get(name);
+                if(result != null) return result;
+            }
+
+            Object variables = map.get(VARIABLES_KEY);
+            if (variables instanceof Map) {
+                return ((Map) variables).get(name);
+            }
+
+            return null;
+        }
+
+        public void setProperty(Map context, Object target, Object name, Object value)
+                throws OgnlException {
+            Map map = (Map) target;
+            map.put(name, value);
+        }
+
+    }
+
+    static class ContextMap extends HashMap<String, Object> {
+
+        public ContextMap(MetaObject parameterMetaObject) {
+            this.parameterMetaObject = parameterMetaObject;
+        }
+
+        @Override
+        public Object get(Object key) {
+            String strKey = (String) key;
+            if (super.containsKey(strKey)) {
+                return super.get(strKey);
+            }
+
+            if (parameterMetaObject != null) {
+                Object object = parameterMetaObject.getValue(strKey);
+                // issue #61 do not modify the context when reading
 //        if (object != null) { 
 //          super.put(strKey, object);
 //        }
 
-        return object;
-      }
+                return object;
+            }
 
-      return null;
-    }
-  }
+            return null;
+        }
 
-  static class ContextAccessor implements PropertyAccessor {
+        @Override
+        public Object put(String key, Object value) {
+            return super.put(key, value);
+        }
 
-    public Object getProperty(Map context, Object target, Object name)
-        throws OgnlException {
-      Map map = (Map) target;
+        private MetaObject parameterMetaObject;
 
-      Object result = map.get(name);
-      if (result != null) {
-        return result;
-      }
+        private static final long serialVersionUID = 2977601501966151582L;
 
-      Object parameterObject = map.get(PARAMETER_OBJECT_KEY);
-      if (parameterObject instanceof Map) {
-    	  return ((Map)parameterObject).get(name);
-      }
-
-      return null;
     }
 
-    public void setProperty(Map context, Object target, Object name, Object value)
-        throws OgnlException {
-      Map map = (Map) target;
-      map.put(name, value);
-    }
-  }
 }
